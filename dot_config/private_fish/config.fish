4e60e5968b84c3760a2c78c2075ce2319a79bf07
Max <687718+MystaMax@users.noreply.github.com>
2026-02-17 18:03:51 -0500

    Initial dotfiles 🎉

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/mystamax/.lmstudio/bin
+# End of LM Studio CLI section
+
